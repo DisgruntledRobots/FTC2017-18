37195f6aaa62ccae24ee5f8bd5300b29c584099c
DisgruntledRobots <disgruntledrobots@gmail.com>
2017-10-16 23:18:59 -0500

    Init
    First commit for FTC season 2017-18.

---
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveUtils/EncoderDriver.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.DriveUtils;
+
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by 5815-Disgruntled on 10/16/2017.
+ */
+
+public class EncoderDriver {
+
+    static final double COUNTS_PER_MOTOR_REV = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 2.0 ;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0 ;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public EncoderDriver(DcMotor[] motors) {
+
+        frontLeft = motors[0];
+        frontRight = motors[1];
+        backLeft = motors[2];
+        backRight = motors[3];
+
+    }
+
+
+    /**
+     * Determines direction based on sign of distance parameter: postive = forwards
+     * negative = backwards. Distance is in inches.
+     * */
+    public void linear(double speed, double distance) {
+
+        int newLeftTarget = frontLeft.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+        int newRightTarget = frontRight.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+
+        frontLeft.setTargetPosition(newLeftTarget);
+        backLeft.setTargetPosition(newLeftTarget);
+        frontRight.setTargetPosition(newRightTarget);
+        backRight.setTargetPosition(newRightTarget);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(Math.abs(speed));
+        frontRight.setPower(Math.abs(speed));
+        backLeft.setPower(Math.abs(speed));
+        backRight.setPower(Math.abs(speed));
+
+        while( frontLeft.isBusy() && frontRight.isBusy() ) {
+
+            //do nothing
+
+        }
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+}
---
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareFrame.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.motors.NeveRest40Gearmotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by 5815-Disgruntled on 10/13/2017.
+ */
+
+public class HardwareFrame {
+
+    /* Public OpMode members. */
+    public DcMotor frontRightMotor = null;
+    public DcMotor  backRightMotor = null;
+    public DcMotor frontLeftMotor = null;
+    public DcMotor backLeftMotor = null;
+
+    private HardwareMap hwMap =  null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public HardwareFrame(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        frontRightMotor = hwMap.get(DcMotor.class, "drive_motor_front_right");
+        backRightMotor = hwMap.get(DcMotor.class, "drive_motor_back_right");
+        frontLeftMotor = hwMap.get(DcMotor.class, "drive_motor_front_left");
+        backLeftMotor = hwMap.get(DcMotor.class, "drive_motor_back_left");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+    }
+
+}
