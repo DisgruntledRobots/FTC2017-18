624fe2c71c85ab62cde712903320b6f2d81309c2
DisgruntledRobots <disgruntledrobots@gmail.com>
2017-11-28 14:37:56 -0600

    Continued work on Teleop_Mecanum_New
    Trying new method to base direction off of joystick angle and gyro
    sensor output.

--- teamcode/OpModes/Teleop_Mecanum_New.java
+++ teamcode/OpModes/Teleop_Mecanum_New.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.FTC2017_18.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.FTC2017_18.teamcode.HardwareFrame;
@@ -8,7 +9,7 @@ import org.firstinspires.ftc.FTC2017_18.teamcode.HardwareFrame;
 /**
  * Created by 5815-Disgruntled on 11/25/2017.
  */
-
+@TeleOp(name="Mecanum Test", group="TeleOp")
 public class Teleop_Mecanum_New extends LinearOpMode {
 
 
@@ -16,11 +17,12 @@ public class Teleop_Mecanum_New extends LinearOpMode {
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
     HardwareFrame robot = new HardwareFrame();
-
-    private double multiplyer = 1;
-    private double liftCoef = 1;
-    public static double a = 17.35/2;
-    public static double b = 12.5/2;
+    public float leftDriveStickX;
+    public float leftDriveStickY;
+    public double leftDriveStickAngle;
+    public float rightDriveStickX;
+    public float rightDriveStickY;
+    public double rightDriveStickAngle;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,23 +39,27 @@ public class Teleop_Mecanum_New extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
+            leftDriveStickX = gamepad1.left_stick_x;
+            leftDriveStickY = gamepad1.left_stick_y != 0 ? -gamepad1.left_stick_y : gamepad1.left_stick_y;
+            leftDriveStickAngle = Math.toDegrees(Math.atan2(leftDriveStickY, leftDriveStickX));
+
+            rightDriveStickX = gamepad1.right_stick_x;
+            rightDriveStickY = gamepad1.right_stick_y != 0 ? -gamepad1.right_stick_y : gamepad1.right_stick_y;
+            rightDriveStickAngle = Math.toDegrees(Math.atan2(rightDriveStickY, rightDriveStickX));
+
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            //telemetry.addData("Drive", gamepad1.left_stick_x);
+
+            //cartesian coordinates and angle of left joystick on gamepad 1
+            telemetry.addData("Left coords: ", "X: " + leftDriveStickX + ", Y: " + leftDriveStickY);
+            telemetry.addData("Left angle: ", leftDriveStickAngle);
+
+            //cartesian coordinates and angle of right joystick on gamepad 1
+            telemetry.addData("Right coords: ", "X: " + rightDriveStickX + ", Y: " + rightDriveStickY);
+            telemetry.addData("Right angle: ", rightDriveStickAngle);
+
+            telemetry.addData("Heading: ", robot.gyroSensor.getHeading());
+
             telemetry.update();
-
-            if( gamepad1.left_stick_y != 0 ) {
-
-                //do nothing
-
-            } else if( gamepad1.right_stick_x != 0 ) {
-
-                //lateralStrafe(gamepad1.right_stick_x);
-
-            }
-
-            telemetry.addData("Debug", "In mecanum drive");
-            telemetry.update();
-
             idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
 
         }
